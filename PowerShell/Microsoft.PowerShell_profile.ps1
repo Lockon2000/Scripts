44b12c5c39ee257408a30150fa2ab152d02dbb1d
Lockon2000 <m.taha2000@outlook.de>
2018-04-08 00:23:49 +0200

    Added color to my PowerShell Prompt

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,12 @@
+function Write-Color([String[]]$Text, [ConsoleColor[]]$Color) {
+    for ($i = 0; $i -lt $Text.Length; $i++) {
+        Write-Host $Text[$i] -Foreground $Color[$i] -NoNewLine
+    }
+}
+
 function prompt {
-    $env:username + "@" + $env:computername + " " + (get-location) + "`n" + "PS> "
+    Write-Color -Text $env:username, "@", $env:computername, " ", (get-location), "`n" -Color Red,DarkCyan,Yellow,White,Green,White
+    "PS> "
 }
 
 New-Alias -Name sublime -Value "C:\Program Files\Sublime Text 3\sublime_text.exe"
