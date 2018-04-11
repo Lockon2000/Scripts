c3f4aad819e34108c9f0d320967de8d4cda89a3a
Lockon2000 <m.taha2000@outlook.de>
2018-04-11 23:40:06 +0200

    Added CleanTemp.ps1

---
+++ PowerShell/CleanTemp.ps1
@@ -0,0 +1,9 @@
+﻿Remove-Item -Path $env:TEMP\* -Recurse -Force
+Remove-Item -Path C:\Windows\Temp\* -Recurse -Force
+Remove-Item -Path C:\Windows\SoftwareDistribution\Download\* -Recurse -Force
+Remove-Item -Path C:\Windows\Prefetch\* -Recurse -Force
+Remove-Item -Path 'C:\Windows\$NT*' -Recurse -Force
+
+vssadmin delete shadows /All /Quiet
+
+cleanmgr /SAGERUN:1
