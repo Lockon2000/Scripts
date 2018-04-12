200b1405c0ae8470f78d2d17a10a25662e2b54f4
Mohamed Abdelwahab <36110739+Lockon2000@users.noreply.github.com>
2018-04-12 16:27:33 +0200

    Added the freed disk space feature and Windows 7 compatibility

---
+++ PowerShell/CleanTemp.ps1
@@ -0,0 +1,37 @@
+﻿$drives = Get-WmiObject win32_logicaldisk
+$total1 = 0
+ForEach ($drive in $drives) {
+    $total1 = $total1 + $drive.FreeSpace
+}
+
+If ((Get-Host).Version.Major -gt 2){
+    Remove-Item -Path $env:TEMP\* -Recurse -Force *>$null
+    Remove-Item -Path C:\Windows\Temp\* -Recurse -Force *>$null
+    Remove-Item -Path C:\Windows\SoftwareDistribution\Download\* -Recurse -Force *>$null
+    Remove-Item -Path C:\Windows\Prefetch\* -Recurse -Force *>$null
+    Remove-Item -Path 'C:\Windows\$NT*' -Recurse -Force *>$null
+
+    vssadmin delete shadows /All /Quiet *>$null
+
+    cleanmgr /SAGERUN:1 | Out-Null
+}
+Else {
+    Remove-Item -Path $env:TEMP\* -Recurse -Force 
+    Remove-Item -Path C:\Windows\Temp\* -Recurse -Force
+    Remove-Item -Path C:\Windows\SoftwareDistribution\Download\* -Recurse -Force
+    Remove-Item -Path C:\Windows\Prefetch\* -Recurse -Force
+    Remove-Item -Path 'C:\Windows\$NT*' -Recurse -Force
+
+    vssadmin delete shadows /All /Quiet | Out-Null
+
+    cleanmgr /SAGERUN:1 | Out-Null
+
+}
+
+$total2 = 0
+ForEach ($drive in $drives) {
+    $total2 = $total2 + $drive.FreeSpace
+}
+
+Write-Host Der freigegebene Speicherplatz beträgt (($total2 - $total1)/(1024*1024)) MBs `n
+Pause
