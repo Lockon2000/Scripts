cf54f867fc2add0838f8289b15f5048c6c3a6977
Mohamed Abdelwahab <mabdelwahab@dtad.de>
2018-06-05 11:14:55 +0200

    Modified BloatwareEntfernen.ps1 and CleanTemp.ps1

--- PowerShell/BloatwareEntfernen.ps1
+++ PowerShell/BloatwareEntfernen.ps1
@@ -5,28 +5,47 @@ If (-Not $CurrentlyAdmin)
     Exit
 }
 
-$packages = "*A278AB0D*","*Duolingo*","*king.com*","46928bounde.EclipseManager","64885BlueEdge.OneCalendar","AdobeSystemsIncorporated.AdobePhotoshopExpress","DolbyLaboratories.DolbyAccess",
-"Microsoft.BingNews","Microsoft.BingTranslator","Microsoft.BingWeather","Microsoft.GetHelp","Microsoft.Messaging","Microsoft.Microsoft3DViewer","Microsoft.MicrosoftOfficeHub",
-"Microsoft.MicrosoftSolitaireCollection","Microsoft.MSPaint","Microsoft.Office.OneNote","Microsoft.Office.Sway","Microsoft.OneConnect","Microsoft.People","Microsoft.Print3D","Microsoft.SkypeApp",
-"Microsoft.Wallet","Microsoft.WindowsCalculator","Microsoft.WindowsFeedbackHub","Microsoft.Xbox.TCUI","Microsoft.XboxApp","Microsoft.XboxApp","Microsoft.XboxGameOverlay","Microsoft.XboxIdentityProvider",
-"Microsoft.XboxSpeechToTextOverlay","Microsoft.ZuneMusic","Microsoft.ZuneVideo","XINGAG.XING","Microsoft.NetworkSpeedTest"
+$packages = "*A278AB0D*","*Duolingo*","*king.com*","46928bounde.EclipseManager","64885BlueEdge.OneCalendar","AdobeSystemsIncorporated.AdobePhotoshopExpress","DolbyLaboratories.DolbyAccess","Microsoft.BingNews","Microsoft.BingTranslator","Microsoft.BingWeather","Microsoft.GetHelp","Microsoft.Messaging","Microsoft.Microsoft3DViewer","Microsoft.MicrosoftOfficeHub","Microsoft.MicrosoftSolitaireCollection","Microsoft.Office.OneNote","Microsoft.Office.Sway","Microsoft.OneConnect","Microsoft.People","Microsoft.Print3D","Microsoft.Wallet","Microsoft.WindowsFeedbackHub","Microsoft.Xbox.TCUI","Microsoft.XboxApp","Microsoft.XboxApp","Microsoft.XboxGameOverlay","Microsoft.XboxIdentityProvider","Microsoft.XboxSpeechToTextOverlay","Microsoft.ZuneMusic","Microsoft.ZuneVideo","XINGAG.XING","Microsoft.NetworkSpeedTest"
+
+$provisions = "*Microsoft.BingWeather*","*Microsoft.Messaging*","*Microsoft.Microsoft3DViewer*","*Microsoft.MicrosoftSolitaireCollection*","*Microsoft.Office.OneNote*","*Microsoft.Print3D*","*Microsoft.MSPaint*","*Microsoft.Wallet*","*Microsoft.WindowsFeedbackHub*","*Microsoft.Xbox.TCUI*","*Microsoft.XboxApp*","*Microsoft.XboxGameOverlay*","*Microsoft.XboxIdentityProvider*","*Microsoft.XboxSpeechToTextOverlay*","*Microsoft.ZuneMusic*","*Microsoft.ZuneVideo*"
 
 Write-Output "Deinstalliere App-Packete"
 
+$i = 1
+[System.Collections.ArrayList]$notuninstalled = @()
 foreach ($p in $packages) {
-$job = Start-Job {param($p); Get-AppxPackage $p | Remove-AppxPackage } -ArgumentList $p
-Wait-Job $job
-Write-Output (Receive-Job $job)
+    $job = Start-Job {param($p); Get-AppxPackage $p | Remove-AppxPackage } -ArgumentList $p
+    Wait-Job $job | Out-Null
+    if (-not (Get-AppxPackage $p)) {
+        Write-Output "$i von $($packages.count)"
+        $i++
+    }
+    else {
+        $notuninstalled += $p
+    }
 }
 
-$provisions = "*Microsoft.BingWeather*","*Microsoft.Messaging*","*Microsoft.Microsoft3DViewer*","*Microsoft.MicrosoftSolitaireCollection*","*Microsoft.Office.OneNote*","*Microsoft.Print3D*","*Microsoft.MSPaint*",
-"*Microsoft.Wallet*","*Microsoft.WindowsFeedbackHub*","*Microsoft.Xbox.TCUI*","*Microsoft.XboxApp*","*Microsoft.XboxGameOverlay*","*Microsoft.XboxIdentityProvider*","*Microsoft.XboxSpeechToTextOverlay*",
-"*Microsoft.ZuneMusic*","*Microsoft.ZuneVideo*"
+if ($notuninstalled) {
+    Write-Output "Folgende Apps wurden nicht deinstalliert: " $notuninstalled
+}
 
 Write-Output "Deinstalliere App-Provisionen"
 
+$i = 1
+[System.Collections.ArrayList]$notuninstalled = @()
 foreach ($p in $provisions) {
-Get-appxprovisionedpackage –online | where-object {$_.packagename –like $p} | remove-appxprovisionedpackage –online *>$null
+    Get-appxprovisionedpackage –online | where-object {$_.packagename –like $p} | remove-appxprovisionedpackage –online *>$null
+    if (-not (Get-appxprovisionedpackage –online | where-object {$_.packagename –like $p})) {
+        Write-Output "$i von $($provisions.count)"
+        $i++
+    }
+    else {
+        $notuninstalled += $p
+    }
+}
+
+if ($notuninstalled) {
+    Write-Output "Folgende Provisionen wurden nicht entfernt: " $notuninstalled
 }
 
 Write-Output "Fertig!"
