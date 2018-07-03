13c83309e6e7b785fae26963711a082eac2d5a4b
Mohamed Abdelwahab <mabdelwahab@dtad.de>
2018-07-03 17:44:17 +0200

    Added win7 capabilities to Dateiexplorer-Einstellungen.ps1

--- PowerShell/Dateiexplorer-Einstellungen (win7).cmd
+++ PowerShell/Dateiexplorer-Einstellungen (win7).cmd
@@ -1,1 +1,3 @@
+@Echo off
 Powershell.exe -NoLogo -ExecutionPolicy Bypass -File "Dateiexplorer-Einstellungen.ps1"
+@Echo on
--- PowerShell/Dateiexplorer-Einstellungen.ps1
+++ PowerShell/Dateiexplorer-Einstellungen.ps1
@@ -1,9 +1,3 @@
-$CurrentlyAdmin = (New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-if (-not $CurrentlyAdmin)
-{
-    Start-Process Powershell.exe -Verb runAs -ArgumentList "-NoLogo -ExecutionPolicy Bypass -File `"$PSCommandPath`""
-}
-
 $ExplorerAdvancedKey = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
 $ExplorerCabinetStateKey = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\CabinetState\'
 Set-ItemProperty $ExplorerAdvancedKey Hidden 1
@@ -17,4 +11,4 @@ Set-ItemProperty $ExplorerAdvancedKey NavPaneExpandToCurrentFolder 1
 Set-ItemProperty $ExplorerCabinetStateKey FullPath 1
 
 Write-Output "Registrykeys wurden geändert!"
-Pause
+Read-Host -Prompt "Drücke Enter um zu beenden..."
