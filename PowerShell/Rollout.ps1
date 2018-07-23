bebc0cadf53f9e8b974da7aba5c7b2296ea316ab
Mohamed Abdelwahab <mabdelwahab@dtad.de>
2018-07-23 13:39:19 +0200

    Added the 'private' option to Rollout.ps1

--- PowerShell/Rollout.ps1
+++ PowerShell/Rollout.ps1
@@ -1,9 +1,11 @@
-param([Parameter(Mandatory=$true)] [string]$commitMessage)
+param([Parameter(Mandatory=$true)] [string]$commitMessage, [bool]$private)
 
 
 Copy-Item -Path "C:\Users\mabdelwahab\Arbeit\PowerShell-Skripte\*" -Destination "C:\Users\mabdelwahab\Arbeit\Scripts\PowerShell" -Exclude "*.md"
-Copy-Item -Path "C:\Users\mabdelwahab\Arbeit\PowerShell-Skripte\*" -Destination "\\odin\it\software\Skripte" -Exclude "*.md"
-Copy-Item -Path "C:\Users\mabdelwahab\Arbeit\PowerShell-Skripte\*" -Destination "E:\Skripte" -Exclude "*.md"
+if (-not $private) {
+    Copy-Item -Path "C:\Users\mabdelwahab\Arbeit\PowerShell-Skripte\*" -Destination "\\odin\it\software\Skripte" -Exclude "*.md"
+    Copy-Item -Path "C:\Users\mabdelwahab\Arbeit\PowerShell-Skripte\*" -Destination "E:\Skripte" -Exclude "*.md"
+}
 
 Write-Output "Begin with commiting..."
 
@@ -19,4 +21,4 @@ git add .
 git commit -m $commitMessage
 git push
 
-Write-Output "Finished!!"
+Write-Output "`nFinished!!"
