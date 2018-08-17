a5a3eb75de95630abb46c1cf98af9b7fc7aea28d
Lockon2000 <m.taha2000@outlook.de>
2018-08-17 10:42:52 +0200

    Added the Exclue parameter to DTADLib.psm1

--- PowerShell/DTADLib.psm1
+++ PowerShell/DTADLib.psm1
@@ -2,18 +2,24 @@ $cred = Import-Clixml -Path C:\Users\mabdelwahab\Arbeit\Administration\cred.xml
 
 
 function Get-Clients {
-    param([string[]]$Department="all", [string]$PCType="all")
+    param([string[]]$Department="all", [string]$PCType="all", [string[]]$Exclude)
     [System.Collections.ArrayList]$list = @()
 
     if ("all" -in $Department) {
         foreach ($file in (dir 'C:\Users\mabdelwahab\Arbeit\Administration\PowerShell Clients')) {
-            foreach ($pc in (Get-Content $file.FullName)) {
-                $pc = $pc -split " "
-                if ($PCType -eq "all") {
-                    $list += $pc[0]
-                } else {
-                    if ($pc[1] -eq $PCType) {
-                        $list += $pc[0]
+            $dep = $file.name.Substring(7,$file.name.Length-11)
+            if ($dep -notin $Exclude) {
+                foreach ($pc in (Get-Content $file.FullName)) {
+                    $pc = $pc -split " "
+
+                    if ($pc[0] -notin $Exclude) {
+                        if ($PCType -eq "all") {
+                            $list += $pc[0]
+                        } else {
+                            if ($pc[1] -eq $PCType) {
+                                $list += $pc[0]
+                            }
+                        }
                     }
                 }
             }
@@ -22,11 +28,14 @@ function Get-Clients {
         foreach ($dep in $Department) {
             foreach ($pc in (Get-Content "C:\Users\mabdelwahab\Arbeit\Administration\PowerShell Clients\Clients$dep.txt")) {
                 $pc = $pc -split " "
-                if ($PCType -eq "all") {
-                    $list += $pc[0]
-                } else {
-                    if ($pc[1] -eq $PCType) {
+
+                if ($pc[0] -notin $Exclude) {
+                    if ($PCType -eq "all") {
                         $list += $pc[0]
+                    } else {
+                        if ($pc[1] -eq $PCType) {
+                            $list += $pc[0]
+                        }
                     }
                 }
             }
@@ -36,5 +45,7 @@ function Get-Clients {
     return $list
 }
 
+New-Alias gcli -Value Get-Clients
 
-Export-ModuleMember -Variable * -Function *
+
+Export-ModuleMember -Variable * -Function * -Alias *
--- PowerShell/Rollout.ps1
+++ PowerShell/Rollout.ps1
@@ -1,10 +1,10 @@
 param([Parameter(Mandatory=$true)] [string]$CommitMessage, [switch]$Private, [switch]$NoCommit, [switch]$NoPush)
 
 
-Copy-Item -Path "C:\Users\mabdelwahab\Arbeit\PowerShell-Skripte\*" -Destination "C:\Users\mabdelwahab\Arbeit\Scripts\PowerShell" -Exclude "*.md"
+Copy-Item -Path "C:\Users\mabdelwahab\Arbeit\PowerShell-Skripte\*" -Destination "C:\Users\mabdelwahab\Arbeit\Scripts\PowerShell" -Exclude "*.md" -PassThru
 if (-not $Private) {
-    Copy-Item -Path "C:\Users\mabdelwahab\Arbeit\PowerShell-Skripte\*" -Destination "\\odin\it\software\Skripte" -Exclude "*.md"
-    Copy-Item -Path "C:\Users\mabdelwahab\Arbeit\PowerShell-Skripte\*" -Destination "E:\Skripte" -Exclude "*.md"
+    Copy-Item -Path "C:\Users\mabdelwahab\Arbeit\PowerShell-Skripte\*" -Destination "\\odin\it\software\Skripte" -Exclude "*.md" -PassThru
+    Copy-Item -Path "C:\Users\mabdelwahab\Arbeit\PowerShell-Skripte\*" -Destination "E:\Skripte" -Exclude "*.md" -PassThru
 }
 
 
