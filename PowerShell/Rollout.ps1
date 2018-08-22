107408ca539a2b9e4ea89264551cb997aa8e9c2c
Lockon2000 <m.taha2000@outlook.de>
2018-08-22 11:36:03 +0200

    Made the Rollout script not copy itself to the Public Folders

--- PowerShell/Rollout.ps1
+++ PowerShell/Rollout.ps1
@@ -3,20 +3,21 @@ param([Parameter(Mandatory=$true)] [string]$CommitMessage, [switch]$Private, [sw
 
 Copy-Item -Path "C:\Users\mabdelwahab\Arbeit\PowerShell-Skripte\*" -Destination "C:\Users\mabdelwahab\Arbeit\Scripts\PowerShell" -Exclude "*.md" -PassThru
 if (-not $Private) {
-    Copy-Item -Path "C:\Users\mabdelwahab\Arbeit\PowerShell-Skripte\*" -Destination "\\odin\it\admin\Skripte" -Exclude "*.md" -PassThru
+    Copy-Item -Path "C:\Users\mabdelwahab\Arbeit\PowerShell-Skripte\*" -Destination "\\odin\it\admin\Skripte" -Exclude "README.md","Rollout.ps1" -PassThru
     if (-not $NoUSB) {
-        Copy-Item -Path "C:\Users\mabdelwahab\Arbeit\PowerShell-Skripte\*" -Destination "E:\Skripte" -Exclude "*.md" -PassThru
+        Copy-Item -Path "C:\Users\mabdelwahab\Arbeit\PowerShell-Skripte\*" -Destination "E:\Skripte" -Exclude "README.md","Rollout.ps1" -PassThru
     }
 }
 
+Write-Output "`n"
 
 if (-not $NoCommit) {
     Set-Location "C:\Users\mabdelwahab\Arbeit\PowerShell-Skripte"
     git add .
-    Write-Output "Commiting"
+    Write-Output "=== Commiting ==="
     git commit -m $CommitMessage
     if (-not $NoPush) {
-        Write-Output "`nPushing"
+        Write-Output "`n=== Pushing ==="
         git push
     }
 
@@ -24,10 +25,10 @@ if (-not $NoCommit) {
 
     Set-Location "C:\Users\mabdelwahab\Arbeit\Scripts\PowerShell"
     git add .
-    Write-Output "Commiting"
+    Write-Output "=== Commiting ==="
     git commit -m $CommitMessage
     if (-not $NoPush) {
-        Write-Output "`nPushing"
+        Write-Output "`n=== Pushing ==="
         git push
     }
 }
