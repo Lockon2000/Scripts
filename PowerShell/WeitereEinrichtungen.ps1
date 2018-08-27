34fdf6451723d954d6a2cf1f83f04d0bd2fdc49a
Lockon2000 <m.taha2000@outlook.de>
2018-08-27 12:29:54 +0200

    Made sure all Scripts are using the correct version of Session Elevating commands

--- PowerShell/WeitereEinrichtungen.ps1
+++ PowerShell/WeitereEinrichtungen.ps1
@@ -1,3 +1,10 @@
+$CurrentlyAdmin = (New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+If (-Not $CurrentlyAdmin)
+{
+    Start-Process powershell.exe -Verb runAs -ArgumentList "-NoLogo -ExecutionPolicy Bypass -File `"$PSCommandPath`""
+    Exit
+}
+
 Write-Output "Lasse Pings zu:"
 netsh advfirewall firewall add rule name="Enable Echo Ping Request" protocol="icmpv4:8,any" dir=in action=allow
 
