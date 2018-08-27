20363f459db605a01941302f5142eb92f8d9af39
Lockon2000 <m.taha2000@outlook.de>
2018-08-27 23:25:15 +0200

    Made the Elevate-PSSession function preserve the directory

--- PowerShell/ZugangsdatenErstellen.ps1
+++ PowerShell/ZugangsdatenErstellen.ps1
@@ -1,7 +1,7 @@
 $Cred = @{}
 $Cred.Admin = @{}
-$Cred.Admin.Desktop = Get-Credential -UserName admin -Message "Gib bitte das Admin-Kennwort fÃ¼r Desktops ein!"
-$Cred.Admin.Laptop = Get-Credential -User admin -Message "Gib bitte das Admin-Kennwort fÃ¼r Laptops ein!"
-$Cred.User = Get-Credential -UserName user -Message "Gib bitte das User-Kennwort fÃ¼r Desktops und Laptops ein!"
+$Cred.Admin.Desktop = Get-Credential -UserName admin -Message "Gib bitte das Admin-Kennwort für Desktops ein!"
+$Cred.Admin.Laptop = Get-Credential -User admin -Message "Gib bitte das Admin-Kennwort für Laptops ein!"
+$Cred.User = Get-Credential -UserName user -Message "Gib bitte das User-Kennwort für Desktops und Laptops ein!"
 
 Export-Clixml -InputObject $Cred -Path "C:\Users\$env:Username\Documents\WindowsPowerShell\cred.xml"
