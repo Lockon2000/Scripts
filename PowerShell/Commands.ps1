8dda66343499f55b25e5911231de28e177ffcbd3
Lockon2000 <m.taha2000@outlook.de>
2018-12-12 11:33:17 +0100

    Added a command for Programmatically making a shortcut start with admin privilages in commands.ps1

--- PowerShell/Commands.ps1
+++ PowerShell/Commands.ps1
@@ -12,7 +12,7 @@ If (-Not $CurrentlyAdmin)
 
 
 # To change a service startup type: 0 for instantly and 1 for delayed
-Set-ItemProperty -Path "Registry::HKLM\System\CurrentControlSet\Services\theservice" -Name "DelayedAutostart" -Value 1 -Type DWORD
+Set-ItemProperty -Path "Registry::HKLM\System\CurrentControlSet\Services\<theservice>" -Name "DelayedAutostart" -Value 1 -Type DWORD
 
 
 # Chocolatey profile
@@ -20,3 +20,10 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+
+# Programmatically make a shortcut start with admin privilages
+$bytes = [System.IO.File]::ReadAllBytes("<path-to-shortcut>")
+$bytes[0x15] = $bytes[0x15] -bor 0x20   #set byte 21 (0x15) bit 6 (0x20) ON
+
+[System.IO.File]::WriteAllBytes("<path-to-shortcut>", $bytes)
